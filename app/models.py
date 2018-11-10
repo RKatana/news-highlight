5b3f65996950765dd013130e7b153a0c3300c1c6
Raphael Katana <roduor41@gmail.com>
2018-11-10 16:20:07 +0300

    initialised the instance of the news in the models file

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class All_news:
+    def __init__(self,name,author,title,description,url,urlToImage,publishedAt,content):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+        
